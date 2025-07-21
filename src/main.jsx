2a0ca2168263c0b46b9437ef0a997316311e93fa
Gautam-Kanojiya <g6kanojiya@gmail.com>
2025-07-21 14:14:59 +0530

    changed app name

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './app.jsx'
 import NavBar from './components/ui/navBar.jsx'
 import Footer from './components/ui/footer.jsx'
 

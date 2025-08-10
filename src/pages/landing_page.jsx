1f99c81a842a9ee8ed9c9649eeb9da8a5e4fb07b
Gurjot Singh <gurjotsingh2004@gmail.com>
2025-08-10 23:37:55 +0530

    Changed the get started to request shipment

--- src/pages/landing_page.jsx
+++ src/pages/landing_page.jsx
@@ -45,7 +45,7 @@ export default function LandingPage() {
                   className="px-6 py-3 transition-all duration-300 transform bg-lxj-accent text-white rounded-full font-semibold hover:bg-lxj-primary"
                 >
                   Get started
-                </button>
+                </button> */}
                 <a
                   href="/about-us"
                   className="px-6 py-3 border-2 transition-all duration-300 transform border-white rounded-full text-white font-semibold hover:bg-white hover:text-lxj-accent"
@@ -56,7 +56,7 @@ export default function LandingPage() {
               </div>
             </div>
 
-            {/* Right Image */}
+            {/* Right Image
             {/* <div className="relative">
               <img
                 src="images/mobile-ui.png"
